45f822e0b18bd5ac20481466b8b17d3221475b1f
gustavnogood <105938111+gustavnogood@users.noreply.github.com>
2024-04-26 15:28:01 +0200

    split the code in smaller fragments

---
+++ dist/assets/axiosInstance-B3xBiW3s.js
@@ -0,0 +1,1 @@
+import{a as e}from"./axios-Bo0ATomq.js";const t="https://bff-competence-test.azurewebsites.net/api/",a=e.create({baseURL:t});export{a};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react': ['react', 'react-dom'],
+          'axios': ['axios'],
+          // Add more chunks as needed
+        },
+      },
+    },
+  },
+};
