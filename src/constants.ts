5d13bf14e6bb4c867ddbe38e8f20d0c9985c138a
gustavnogood <105938111+gustavnogood@users.noreply.github.com>
2024-05-13 14:51:02 +0200

    changed backend url

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 /* export const BACKEND_BASE_URL = "https://bff-competence-test.azurewebsites.net/api/"; */
-export const BACKEND_BASE_URL = "http://localhost:7071/api/"; 
+export const BACKEND_BASE_URL = "https://bff-competence-test.azurewebsites.net/api/"; 
