5bb32005621e420061ccbf7a9f9eb23297f3202b
gustavnogood <105938111+gustavnogood@users.noreply.github.com>
2024-06-04 17:18:28 +0200

    built

---
+++ dist/assets/Login-CphJtCKx.js
@@ -0,0 +1,1 @@
+import{u as e,j as n}from"./index-BK63iK-u.js";import"./react-Da8D78aN.js";const r=()=>{const{instance:i}=e(),t=()=>{i.loginRedirect()};return n.jsx(n.Fragment,{children:n.jsx("button",{onClick:t,children:"Sign in"})})};export{r as default};
